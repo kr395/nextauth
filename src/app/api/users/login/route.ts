e62421533305899b115d0164de1bc4124b7debc3
Gopal Kumar Das <krgopalkumar706@gmail.com>
2025-02-08 19:00:00 +0530

    login filed updated

--- src/app/api/users/login/route.ts
+++ src/app/api/users/login/route.ts
@@ -41,6 +41,7 @@ export async function POST(request: NextRequest) {
 
         const response = NextResponse.json({
           message: "Login Successfull",
+          
           success: true,
         });
 
