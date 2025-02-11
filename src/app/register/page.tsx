28ecc1b63a73f2908a78c1ef864f2f4e8ed0285f
Gopal Kumar Das <krgopalkumar706@gmail.com>
2025-02-11 19:00:00 +0530

    working on Frontend Part. Register page

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,9 +1,49 @@
-import React from 'react'
+"use client";
+import React, { useEffect } from "react";
+import { useState } from "react";
+import axios from "axios";
+import { toast } from "react-hot-toast";
+import { useRouter } from "next/navigation";
 
 const Register = () => {
-  return (
-    <div>Register</div>
-  )
-}
+  const router = useRouter();
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+    username: "",
+  });
 
-export default Register
+  const [canSubmit, setCanSubmit] = React.useState(false);
+  const [loading, setLoading] = React.useState(false);
+
+  const onRegister = async () => {
+    try {
+      setLoading(true);
+      const response = await axios.post("/api/users/register", user);
+      console.log("Signup success", response.data);
+
+      toast.success("Registered successfully");
+      router.push("/login");
+    } catch (error: any) {
+      console.log("Register error", error);
+      toast.error(error.message);
+    }
+  };
+
+  useEffect(() => {
+    if (
+      user.email.length > 0 &&
+      user.password.length > 0 &&
+      user.username.length > 0
+    ) {
+      setCanSubmit(true);
+    } else {
+      setCanSubmit(false);
+    }
+  }, [user]);
+  return( <div className="flex flex-col items-center justify-center min-h-screen py-2">
+     <h1>{loading ? "Processing" : "Input your details"}</h1>
+  </div>);
+};
+
+export default Register;
